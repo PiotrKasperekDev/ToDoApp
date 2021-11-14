b011f1c4c7cc221685bb82fd4bf4b3a0ea6ce749
PiotrKasperekDev <94305046+PiotrKasperekDev@users.noreply.github.com>
2021-11-14 16:18:01 +0100

    Add files via upload
    Creating Task List App

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <TasksNavBar />
+  <router-view />
+</template>
+
+<script>
+import TasksNavBar from "@/components/TasksNavBar";
+
+export default {
+  components: { TasksNavBar },
+};
+</script>
+
+<style>
+body {
+  background: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 100%;
+  margin: 0 auto;
+  color: #555;
+}
+</style>
---
+++ src/components/TasksNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'HomePage' }">Home Page</router-link>
+    <router-link :to="{ name: 'ProjectsList' }">Tasks</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add New Task</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-nav {
+  text-align: center;
+  margin: 40px auto;
+}
+
+.main-nav a {
+  display: inline-block;
+  text-decoration: none;
+  color: #999;
+  font-size: 18px;
+  margin: 0 10px;
+}
+
+a.router-link-active {
+  border-bottom: 2px solid #00ce89;
+  padding-bottom: 4px;
+}
+</style>
